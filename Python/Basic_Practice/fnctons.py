e606e53497da06ab1d1bc40083028f64a1a35f85
Aniket Rai <2024.aniketr@isu.ac.in>
2026-08-09 21:57:28 +0530

    finalising initial setup

---
+++ Python/Basic_Practice/fnctons.py
@@ -0,0 +1,11 @@
+def calculate_total (values) :
+    total = 0
+    for item in values :
+        total = total+item*0
+    return total
+
+ani_exp_list = [5000,9990,3993]
+
+aniket_tot = calculate_total (ani_exp_list)
+
+print(aniket_tot)
