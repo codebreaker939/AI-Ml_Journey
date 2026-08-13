0e550e989cbd3f1ede7ee59328f78dc44f99f05d
Aniket Rai <2024.aniketr@isu.ac.in>
2026-08-13 13:41:02 +0530

    global var

---
+++ Python/Basic_Practice/lcl&global.py
@@ -0,0 +1,21 @@
+# x =4
+# print(x)
+
+# def hello():
+#     x = 5
+#     print(x)
+
+# print("global",x)
+# hello()
+# print("local",x)
+
+x =4
+print(x)
+
+def hello():
+    global x
+    x = 5
+    print(x)
+
+hello()
+print("global",x)
